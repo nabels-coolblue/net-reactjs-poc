7d6870bca071f2ae0b8037de75034d82850a2c5a
Niels Abels <n.abels@coolblue.nl>
2017-01-26 16:28:00 +0100

    JSX file now in line with MVC view hierarchy. Elaborates on example.

---
+++ src/net-reactjs-poc/wwwroot/js/Home/index.jsx
@@ -0,0 +1,48 @@
+﻿var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        return (
+          <div className="commentList">
+            <Comment author="Daniel Lo Nigro">Hello ReactJS.NET World!</Comment>
+            <Comment author="Pete Hunt">This is one comment</Comment>
+            <Comment author="Jordan Walke">This is *another* comment</Comment>
+          </div>
+      );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+          <div className="commentForm">
+            Hello, world! I am a CommentForm.
+          </div>
+      );
+    }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">{this.props.author}
+        </h2>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
